598ad118245bfff920f906c779ba6fd7bfc0b14c
Kyle Coberly <kyle.coberly@gmail.com>
2017-11-26 14:35:54 -0700

    Fixing enum error in sheets

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function addRows(range, data, options){
             spreadsheetId: options.spreadsheetId,
             range,
             valueInputOption: options.valueInputOption || "USER_ENTERED",
-            insertDataOption: "INSERT_DATA",
+            insertDataOption: "INSERT_ROWS",
             resource: {
                 values: data
             }
